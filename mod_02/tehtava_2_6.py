0be7929d3b3704b9a92bf31fa46264740709d80b
Rasmus <rasmus.a.lindstrom@gmail.com>
2025-08-19 23:07:28 +0300

    Moduuli 2 tehtävä 6 valmis

--- mod_02/tehtava_2_6.py
+++ mod_02/tehtava_2_6.py
@@ -1,8 +1,19 @@
 import random
 
-r4num1 = random.randint(1, 9)
-r4num2 = random.randint(1, 9)
-r4num3 = random.randint(1, 9)
-r4num4 = random.randint(1, 9)
+short_loop_counter = 0 
+short_num_list = []
+while  short_loop_counter < 3:
+    temp_var = random.randint(1, 9)
+    short_num_list.append(temp_var)
+    short_loop_counter += 1
 
-print(f"{r4num1}{r4num2}{r4num3}{r4num4}")
+
+long_loop_counter = 0 
+long_num_list = []
+while  long_loop_counter < 4:
+    temp_var = random.randint(1, 6)
+    long_num_list.append(temp_var)
+    long_loop_counter += 1
+
+print(f"Lyhyt numero sarja: {short_num_list}")
+print(f"Pitkä numero sarja: {long_num_list}")
